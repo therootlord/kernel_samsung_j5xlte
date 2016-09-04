b24ff67cdc163bf0cad74a5a2429ecdcd13e4f96
Stephen Hyde <steve1030uk@gmail.com>
2016-09-04 17:28:35 +0100

    fix last commit

--- .missing-syscalls.d
+++
@@ -1,8 +0,0 @@
--:  /build/j500fmm/kernel/include/linux/kconfig.h \
- include/generated/autoconf.h arch/arm/include/generated/asm/types.h \
- /build/j500fmm/kernel/include/uapi/asm-generic/types.h \
- include/asm-generic/int-ll64.h include/uapi/asm-generic/int-ll64.h \
- arch/arm/include/generated/asm/bitsperlong.h \
- include/asm-generic/bitsperlong.h include/uapi/asm-generic/bitsperlong.h \
- /build/j500fmm/kernel/arch/arm/include/asm/unistd.h \
- /build/j500fmm/kernel/arch/arm/include/uapi/asm/unistd.h
--- scripts/mod/.empty.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_scripts/mod/empty.o := /build/j500fn/linaro/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin/arm-none-eabi-gcc -Wp,-MD,scripts/mod/.empty.o.d  -nostdinc -isystem /build/j500fn/linaro/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin/../lib/gcc/arm-none-eabi/4.8.3/include -I/build/j500fmm/kernel/arch/arm/include -Iarch/arm/include/generated  -Iinclude -I/build/j500fmm/kernel/arch/arm/include/uapi -Iarch/arm/include/generated/uapi -I/build/j500fmm/kernel/drivers/soc/qcom -I/build/j500fmm/kernel/include/uapi -Iinclude/generated/uapi -include /build/j500fmm/kernel/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-msm/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -Os -Wno-maybe-uninitialized -fno-dwarf2-cfi-asm -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -marm -D__LINUX_ARM_ARCH__=7 -march=armv7-a -msoft-float -Uarm -Wframe-larger-than=1024 -fstack-protector -Wno-unused-but-set-variable -fomit-frame-pointer -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(empty)"  -D"KBUILD_MODNAME=KBUILD_STR(empty)" -c -o scripts/mod/.tmp_empty.o scripts/mod/empty.c
-
-source_scripts/mod/empty.o := scripts/mod/empty.c
-
-deps_scripts/mod/empty.o := \
-
-scripts/mod/empty.o: $(deps_scripts/mod/empty.o)
-
-$(deps_scripts/mod/empty.o):
