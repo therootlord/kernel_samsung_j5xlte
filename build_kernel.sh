eec2b5c8a57eb7412844335d3d771995db5c223d
Stephen Hyde <steve1030uk@gmail.com>
2016-09-07 12:41:03 +0100

    update git more fixes

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,4 +3,5 @@
 make VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j9
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+tools/dtbTool -s 2048 -o arch/arm/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/ -v
+
