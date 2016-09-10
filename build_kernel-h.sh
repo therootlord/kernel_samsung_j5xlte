f3eccd485a142a66421dc39408433b578a750bca
Stephen Hyde <steve1030uk@gmail.com>
2016-09-10 18:33:04 +0100

    Add Linux 3.10.55 and 3.10.56

--- build_kernel-h.sh
+++ build_kernel-h.sh
@@ -3,3 +3,4 @@
 make VARIANT_DEFCONFIG=msm8916_sec_j53g_eur_defconfig msm8916_sec_h_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j9
 
+tools/dtbTool -s 2048 -o arch/arm/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/ -v
