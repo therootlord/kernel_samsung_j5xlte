a589cc986d3c6f4ee2549a61c4397c87e805f87e
Stephen Hyde <steve1030uk@gmail.com>
2016-09-11 13:02:59 +0100

    linux 3.10.94 and fixes

--- fs/exec.c
+++ fs/exec.c
@@ -19,7 +19,7 @@
  * current->executable is only used by the procfs.  This allows a dispatch
  * table to check for several different types  of binary formats.  We keep
  * trying until we recognize the file or we run out of supported binary
- * formats.
+ * formats. 
  */
 
 #include <linux/slab.h>
@@ -55,7 +55,6 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/compat.h>
-#include <linux/ksm.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1140,7 +1139,7 @@ void setup_new_exec(struct linux_binprm * bprm)
 	   group */
 
 	current->self_exec_id++;
-
+			
 	flush_signal_handlers(current, 0);
 	do_close_on_exec(current->files);
 }
@@ -1299,53 +1298,6 @@ static void bprm_fill_uid(struct linux_binprm *bprm)
 
 	/* We ignore suid/sgid if there are no mappings for them in the ns */
 	if (!kuid_has_mapping(bprm->cred->user_ns, uid) ||
-			!kgid_has_mapping(bprm->cred->user_ns, gid))
-		return;
-
-	if (mode & S_ISUID) {
-		bprm->per_clear |= PER_CLEAR_ON_SETID;
-		bprm->cred->euid = uid;
-	}
-
-	if ((mode & (S_ISGID | S_IXGRP)) == (S_ISGID | S_IXGRP)) {
-		bprm->per_clear |= PER_CLEAR_ON_SETID;
-		bprm->cred->egid = gid;
-	}
-}
-
-static void bprm_fill_uid(struct linux_binprm *bprm)
-{
-	struct inode *inode;
-	unsigned int mode;
-	kuid_t uid;
-	kgid_t gid;
-
-	/* clear any previous set[ug]id data from a previous binary */
-	bprm->cred->euid = current_euid();
-	bprm->cred->egid = current_egid();
-
-	if (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID)
-		return;
-
-	if (current->no_new_privs)
-		return;
-
-	inode = file_inode(bprm->file);
-	mode = ACCESS_ONCE(inode->i_mode);
-	if (!(mode & (S_ISUID|S_ISGID)))
-		return;
-
-	/* Be careful if suid/sgid is set */
-	mutex_lock(&inode->i_mutex);
-
-	/* reload atomically mode/uid/gid now that lock held */
-	mode = inode->i_mode;
-	uid = inode->i_uid;
-	gid = inode->i_gid;
-	mutex_unlock(&inode->i_mutex);
-
-	/* We ignore suid/sgid if there are no mappings for them in the ns */
-	if (!kuid_has_mapping(bprm->cred->user_ns, uid) ||
 		 !kgid_has_mapping(bprm->cred->user_ns, gid))
 		return;
 
@@ -1374,6 +1326,7 @@ int prepare_binprm(struct linux_binprm *bprm)
 		return -EACCES;
 
 	bprm_fill_uid(bprm);
+
 	/* fill in binprm security blob */
 	retval = security_bprm_set_creds(bprm);
 	if (retval)
@@ -1517,132 +1470,6 @@ int search_binary_handler(struct linux_binprm *bprm)
 }
 
 EXPORT_SYMBOL(search_binary_handler);
-
-#if defined CONFIG_SEC_RESTRICT_FORK
-#if defined CONFIG_SEC_RESTRICT_ROOTING_LOG
-#define PRINT_LOG(...)	printk(KERN_ERR __VA_ARGS__)
-#else
-#define PRINT_LOG(...)
-#endif	// End of CONFIG_SEC_RESTRICT_ROOTING_LOG
-
-#define CHECK_ROOT_UID(x) (x->cred->uid == 0 || x->cred->gid == 0 || \
-			x->cred->euid == 0 || x->cred->egid == 0 || \
-			x->cred->suid == 0 || x->cred->sgid == 0)
-
-/*  sec_check_execpath
-    return value : give task's exec path is matched or not
-*/
-int sec_check_execpath(struct mm_struct *mm, char *denypath)
-{
-	struct file *exe_file;
-	char *path, *pathbuf = NULL;
-	unsigned int path_length = 0, denypath_length = 0;
-	int ret = 0;
-
-	if (mm == NULL)
-		return 0;
-
-	if (!(exe_file = get_mm_exe_file(mm))) {
-		PRINT_LOG("Cannot get exe from task->mm.\n");
-		goto out_nofile;
-	}
-
-	if (!(pathbuf = kmalloc(PATH_MAX, GFP_TEMPORARY))) {
-		PRINT_LOG("failed to kmalloc for pathbuf\n");
-		goto out;
-	}
-
-	path = d_path(&exe_file->f_path, pathbuf, PATH_MAX);
-	if (IS_ERR(path)) {
-		PRINT_LOG("Error get path..\n");
-		goto out;
-	}
-
-	path_length = strlen(path);
-	denypath_length = strlen(denypath);
-
-	if (!strncmp(path, denypath, (path_length < denypath_length) ?
-				path_length : denypath_length)) {
-		ret = 1;
-	}
-out:
-	fput(exe_file);
-out_nofile:
-	if (pathbuf)
-		kfree(pathbuf);
-
-	return ret;
-}
-EXPORT_SYMBOL(sec_check_execpath);
-
-static int sec_restrict_fork(void)
-{
-	struct cred *shellcred;
-	int ret = 0;
-	struct task_struct *parent_tsk;
-	struct mm_struct *parent_mm = NULL;
-	const struct cred *parent_cred;
-
-	read_lock(&tasklist_lock);
-	parent_tsk = current->parent;
-	if (!parent_tsk) {
-		read_unlock(&tasklist_lock);
-		return 0;
-	}
-
-	get_task_struct(parent_tsk);
-	/* holding on to the task struct is enough so just release
-	 * the tasklist lock here */
-	read_unlock(&tasklist_lock);
-
-	if (current->pid == 1 || parent_tsk->pid == 1)
-		goto out;
-
-	/* get current->parent's mm struct to access it's mm
-	 * and to keep it alive */
-	parent_mm = get_task_mm(parent_tsk);
-
-	if (current->mm == NULL || parent_mm == NULL)
-		goto out;
-
-	if (sec_check_execpath(parent_mm, "/sbin/adbd")) {
-		shellcred = prepare_creds();
-		if (!shellcred) {
-			ret = 1;
-			goto out;
-		}
-
-		shellcred->uid = 2000;
-		shellcred->gid = 2000;
-		shellcred->euid = 2000;
-		shellcred->egid = 2000;
-		commit_creds(shellcred);
-		ret = 0;
-		goto out;
-	}
-
-	if (sec_check_execpath(current->mm, "/data/")) {
-		ret = 1;
-		goto out;
-	}
-
-	parent_cred = get_task_cred(parent_tsk);
-	if (!parent_cred)
-		goto out;
-	if (!CHECK_ROOT_UID(parent_tsk))
-	{
-		ret = 1;
-	}
-	put_cred(parent_cred);
-out:
-	if (parent_mm)
-		mmput(parent_mm);
-	put_task_struct(parent_tsk);
-
-	return ret;
-}
-#endif	/* End of CONFIG_SEC_RESTRICT_FORK */
-
 
 /*
  * sys_execve() executes a new program.
@@ -1881,19 +1708,6 @@ SYSCALL_DEFINE3(execve,
 	struct filename *path = getname(filename);
 	int error = PTR_ERR(path);
 	if (!IS_ERR(path)) {
-
-#if defined CONFIG_SEC_RESTRICT_FORK
-		if(CHECK_ROOT_UID(current)){
-			if(sec_restrict_fork()){
-				PRINT_LOG("Restricted making process. PID = %d(%s) "
-								"PPID = %d(%s)\n",
-				current->pid, current->comm,
-				current->parent->pid, current->parent->comm);
-				return -EACCES;
-			}
-		}
-#endif	// End of CONFIG_SEC_RESTRICT_FORK
-
 		error = do_execve(path->name, argv, envp);
 		putname(path);
 	}
