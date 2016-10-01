a9ad6cdb937c48b7c50a4c3d749dcb43345a5da8
Surendar karka <sukark@codeaurora.org>
2016-10-01 15:00:23 +0530

    ASoC: msm: qdsp6v2: add AVS 2.8 support in native drivers
    Add AVS 2.8 support in compress driver and Q6 core driver.
    
    Change-Id: Ifb8208e44400475213321529cb1dd060a5b92e93
    Signed-off-by: Surendar karka <sukark@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -242,6 +242,7 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 		 */
 		switch (q6core_get_avs_version()) {
 		case Q6_SUBSYS_AVS2_7:
+		case Q6_SUBSYS_AVS2_8:
 			avg_vol = (volume_l + volume_r) / 2;
 			for (i = 0; i < prtd->num_channels; i++)
 				gain_list[i] = avg_vol;
@@ -260,6 +261,7 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 	} else {
 		switch (q6core_get_avs_version()) {
 		case Q6_SUBSYS_AVS2_7:
+		case Q6_SUBSYS_AVS2_8:
 			gain_list[0] = volume_l;
 			gain_list[1] = volume_r;
 			/* force sending FR/FL/FC volume for mono */
@@ -1737,6 +1739,7 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 			 */
 			switch (q6core_get_avs_version()) {
 			case (Q6_SUBSYS_AVS2_7):
+			case (Q6_SUBSYS_AVS2_8):
 			{
 				q6asm_get_session_time(prtd->audio_client,
 						&prtd->marker_timestamp);
@@ -1915,6 +1918,7 @@ static int msm_compr_pointer(struct snd_compr_stream *cstream,
 
 		switch (q6core_get_avs_version()) {
 		case (Q6_SUBSYS_AVS2_7):
+		case (Q6_SUBSYS_AVS2_8):
 		{
 			rc = q6asm_get_session_time(prtd->audio_client,
 						    &timestamp);
