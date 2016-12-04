d6ad9bae7fa36c7f264773782380be07d96419de
therootlord <igor_cestari@hotmail.com>
2016-12-04 19:37:21 +0000

    Initial commit

---
+++ build_kernel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+
+make msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j
